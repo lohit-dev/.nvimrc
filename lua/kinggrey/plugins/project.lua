8c76366f48e767a1d5bec665d04853d5dc78766c
lohit-dev <lohitsaidev@gmail.com>
2026-07-04 21:26:57 +0530

    revert back to project.nvim

--- lua/kinggrey/plugins/project.lua
+++ lua/kinggrey/plugins/project.lua
@@ -1,16 +1,13 @@
 return {
-  "DrKJeff16/project.nvim", -- fork of the abandoned ahmedkhalf/project.nvim, no
-  -- deprecated APIs, actively maintained
+  "ahmedkhalf/project.nvim",
   event = "VeryLazy",
   opts = {
-    -- detection_methods no longer exists in this fork -- lsp detection is now
-    -- its own table, pattern detection is always on via `patterns` below
-    lsp = { enabled = true },
+    detection_methods = { "lsp", "pattern" },
     patterns = { ".git", "package.json", "pyproject.toml", "Cargo.toml", "go.mod", ".luarc.json" },
     silent_chdir = true,
     scope_chdir = "global",
   },
   config = function(_, opts)
-    require("project").setup(opts) -- module renamed from project_nvim -> project
+    require("project_nvim").setup(opts)
   end,
 }
